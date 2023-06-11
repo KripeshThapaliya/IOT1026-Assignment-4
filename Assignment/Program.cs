efb00939bf0871a0df7dc41de5ebb8f8403e916b
Kripesh Thapaliya <Kripeshthapaliya@gmail.com>
2023-06-11 01:11:12 -0400

    format

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Assignment
+﻿namespace Assignment;
+
+static class Program
 {
-    static class Program
+    static void Main()
     {
-        static void Main()
-        {
-            // Run your RobotTester class here -> RobotTester.TestRobot()
-        }
+        RobotTester.TestRobot();
+    }
+}
+
+public class RobotTester
+{
+    public static void TestRobot()
+    {
+        // Tried using ChatGPT because ran into some 
+        Robot robot = new Robot();
+
+        // Perform various robot commands
+        robot.ExecuteCommand(new OnCommand());
+        robot.ExecuteCommand(new EastCommand());
+        robot.ExecuteCommand(new SouthCommand());
+        robot.ExecuteCommand(new CustomCommand());
+
+        // Display the final state of the robot
+        Console.WriteLine($"Robot state - X: {robot.X}, Y: {robot.Y}, IsPowered: {robot.IsPowered}");
     }
 }
